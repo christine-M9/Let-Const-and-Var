69474dfb6095dd7f28bc0801f0be7e5c422ae21c
Christine <christinekhekama99@gmail.com>
2024-06-01 15:19:27 +0300

    let const and var udemy assignment

---
+++ app/app.js
@@ -0,0 +1,12 @@
+
+let language = "English";
+// console.log(language);
+const president = "Ruto";
+const firstName = "Christine";
+// console.log(firstName);
+firstName = "Khekame";
+console.log(language);
+console.log(president);
+console.log(firstName);
+
+// comments(TypeError: Assignment to constant variable )
